8337945213573e5ebc994b46ec6eb37ff567007d
quivt4125 <vanquythaicute@gmail.com>
2025-08-24 01:09:19 +0700

    fixxx lần nữa
    (cherry picked from commit 8e30314d701be8590960bd3ce142369fe9def9af)

--- config/emailWorker.js
+++ config/emailWorker.js
@@ -1,39 +1,30 @@
-require('dotenv').config();
 const emailQueue = require('../config/emailQueue');
 const nodemailer = require('nodemailer');
-const PromotionUserModel = require('../models/promotionUsersModel'); 
 
-emailQueue.on('completed', (job) => {
-  console.log('[EMAIL] completed:', job.data.to);
-});
-emailQueue.on('failed', (job, err) => {
-  console.error('[EMAIL] failed:', job?.data?.to, err?.message);
-});
+emailQueue.process(async (job, done) => {
+  try {
+    const { to, subject, html } = job.data;
 
-emailQueue.process(async (job) => {
-  const { to, subject, html, userId, promotionId } = job.data;
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
 
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+    await transporter.sendMail({
+      from: `"TIMEMASTERS" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html,
+    });
 
-  await transporter.verify();
-
-  await transporter.sendMail({
-    from: `"TIMEMASTERS" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html,
-  });
-
-  await PromotionUserModel.update(
-    { email_sent: true },
-    { where: { user_id: userId, promotion_id: promotionId } }
-  );
+    done();
+  } catch (err) {
+    console.error('Email worker error:', err.message);
+    done(err);
+  }
 });
